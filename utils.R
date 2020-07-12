e3dd98bef936760ea11abf893e0253e0dd2dc3e3
Marcos Grzeça <marcos-a-g@hotmail.com>
2020-07-12 14:27:02 -0300

    GET-os

--- utils.R
+++ utils.R
@@ -1,6 +1,22 @@
 library(magrittr)
 library(tm)
 
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+  os <- sysinf['sysname']
+  if (os == 'Darwin')
+    os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
 csvRead <- function(dir, rows_to_read){
   data = data.table::fread(dir, nrows=rows_to_read)
   return (data)
